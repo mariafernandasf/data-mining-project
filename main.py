2dbefc5247bddd3ecedfaae3b6d85c0912b9d5a6
Maria Fernanda Sampaio Ferreira <ms7073@columbia.edu>
2025-12-11 14:57:42 +0000

    add weights only parameter due to torch upgrade

--- main.py
+++ main.py
@@ -199,7 +199,7 @@ def main(args):
 
     # RESUME FROM CHECKPOINT 
     if args.resume:
-        checkpoint = torch.load(args.resume, map_location='cpu')
+        checkpoint = torch.load(args.resume, map_location='cpu', weights_only = False)
         model_without_ddp.load_state_dict(checkpoint['model'])
         if not args.eval and 'optimizer' in checkpoint and 'lr_scheduler' in checkpoint and 'epoch' in checkpoint:
             optimizer.load_state_dict(checkpoint['optimizer'])
